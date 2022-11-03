3db2dfcf7073640300a5fcaa438618a94c2d85bd
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-11-03 19:13:23 -0300

    pagina aprimorada

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,23 +1,35 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio).play();
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+
+    else {
+        console.log('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-
-
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0;contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-    
-    contador += 1;
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
