2d6967286687fb19752553080aa87effb63bf215
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-11-03 16:25:17 -0300

    proposta do desafio pronta

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,1 +1,23 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+    
+    contador += 1;
+}
