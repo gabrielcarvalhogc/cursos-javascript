430af0a3850cc5226ca2b74107b759e71421e9cb
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-12-02 11:41:36 -0300

    validação de dados

--- introducao-javascript-na-web/js/principal.js
+++ introducao-javascript-na-web/js/principal.js
@@ -7,9 +7,25 @@ function calculaIMC() {
     var peso = paciente.querySelector(".info-peso").textContent;
     var altura = paciente.querySelector(".info-altura").textContent;
     var tdImc = paciente.querySelector(".info-imc");
+
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;    
+        peso.textContent = "peso inválido!";
+    }
+
+    if(altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;    
+        altura.textContent = "altura inválida!";
+    }
     
-    imc = peso / (altura * altura);
-    tdImc.textContent = imc;
+    if(alturaEhValida && pesoEhValido) {
+        imc = peso / (altura * altura);
+        tdImc.textContent = imc;
+    }
 }
 
 calculaIMC();
