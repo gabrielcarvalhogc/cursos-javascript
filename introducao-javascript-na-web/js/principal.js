9359da81a5ea94a02deaf3d5d146629823bcb7e9
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-12-06 12:15:41 -0300

    agr podemos adicionar um novo paciente

--- introducao-javascript-na-web/js/principal.js
+++ introducao-javascript-na-web/js/principal.js
@@ -3,8 +3,6 @@ titulo.textContent = 'aparecida nutricionista';
 
 var pacientes = document.querySelectorAll(".paciente");
 
-console.log(pacientes);
-
 function calculaIMC() {
     for(var i = 0; i < pacientes.length; i++) {
         var peso = pacientes[i].querySelector(".info-peso").textContent;
@@ -33,3 +31,38 @@ function calculaIMC() {
 }
 
 calculaIMC();
+
+var botaoAdicionar = document.getElementById("adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    var form = document.querySelector('#form-adiciona');
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector('#tabela-pacientes');
+
+    tabela.appendChild(pacienteTr);
+});
