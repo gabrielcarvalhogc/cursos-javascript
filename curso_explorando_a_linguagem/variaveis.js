fd8dc74f1195f120814999b5e1e52c299647a620
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-12-20 10:30:31 -0300

    Create variaveis.js

---
+++ curso_explorando_a_linguagem/variaveis.js
@@ -0,0 +1,15 @@
+console.log('-----------');
+console.log("Variaveis");
+console.log('-----------');
+const nome = 'Gabriel';
+const idade = 20;
+let ano = 2020;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos.`);
+
+console.log('-----------');
+console.log("operações aritiméticas");
+console.log('-----------');
+
+console.log(2 + 2);
+console.log(2 + 8 * 2);
+console.log((2 + 8)* 2);
