c24678bb92a35c5dbb264e2c01dcc3d80d228d29
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2022-12-28 11:40:00 -0300

    Criada a Lista denegociações

--- javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,19 +4,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
-        event.preventDefault();
 
-        const negociacao = new Negociacao(
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulário();
+
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        )
+            this._inputValor.value);
+    }
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulário() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
