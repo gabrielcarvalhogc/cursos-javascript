4e0d8cb595ed912d4ae02d0b869487efafe321e6
Gabriel Carvalho <108549778+gabrielcarvalhogc@users.noreply.github.com>
2023-01-04 15:39:29 -0300

    Update NegociacoesView.js

--- javascript-avancado/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript-avancado/aluraframe/client/js/app/views/NegociacoesView.js
@@ -31,6 +31,8 @@ class NegociacoesView {
             </tbody>
             
             <tfoot>
+                <td colspan="3"></td>
+                <td>${model.negociacoes.reduce((total, n) => total + n.volume, 0.0)}</td>
             </tfoot>
         </table>
         `;
@@ -43,5 +45,5 @@ class NegociacoesView {
 }
 
 /*
-
+    numeros.reduce((total,num) => total * num, 1);
 */
