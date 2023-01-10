c2a755a4534abf11cd3c4a0b69fd6c6bad407aaa
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-01-10 10:55:59 -0300

    try e catch feitos

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,7 +1,21 @@
-async function buscaEndereco() {
-    var consultaCEP = await fetch("https://viacep.com.br/ws/01001000/json/");
-    const consultaCEPConvertido = await consultaCEP.json();
-    console.log(consultaCEPConvertido);
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const consultaCEPConvertido = await consultaCEP.json();
+
+        if (consultaCEPConvertido.erro) {
+            throw Error("CEP não existe!")
+        }
+
+        //console.log(consultaCEPConvertido);
+        return consultaCEPConvertido;
+
+    } catch (erro) {
+        console.log(erro);
+    }
 }
 
-buscaEndereco();
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
