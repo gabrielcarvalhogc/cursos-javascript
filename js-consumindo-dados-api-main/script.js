93708cf85a19030697085b97037af90f24676653
Gabriel Carvalho <gabriel225gc@gmail.com>
2023-01-10 10:39:04 -0300

    mudado para asyinc e await

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,3 +1,7 @@
-var consultaCEP = fetch("https://viacep.com.br/ws/01001000/json/");
+async function buscaEndereco() {
+    var consultaCEP = await fetch("https://viacep.com.br/ws/01001000/json/");
+    const consultaCEPConvertido = await consultaCEP.json();
+    console.log(consultaCEPConvertido);
+}
 
-console.log(consultaCEP);
+buscaEndereco();
